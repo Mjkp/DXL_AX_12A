3192cdb386b5f40dc52fa2ff8133701232284a88
Moon <ucbqkmo@ucl.ac.uk>
2019-11-06 20:28:51 +0000

    second

--- Ax_read_write.py
+++ Ax_read_write.py
@@ -1,21 +1,4 @@
 import os
-'''
-if os.name == 'nt':
-    import msvcrt
-    def getch():
-        return msvcrt.getch().decode()
-else:
-    import sys, tty, termios
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    def getch():
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-'''
 from dynamixel_sdk import *                    # Uses Dynamixel SDK library
 # Control table address
 ADDR_MX_TORQUE_ENABLE      = 24	               # Control table address is different in Dynamixel model
