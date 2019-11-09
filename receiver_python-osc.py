9bf49630ce394fb8792718419d18badd506a594f
Moon <ucbqkmo@ucl.ac.uk>
2019-11-09 22:08:56 +0000

    oscupdate

--- receiver_python-osc.py
+++ receiver_python-osc.py
@@ -1,10 +1,31 @@
+
+import argparse
+import math
+
 from pythonosc import dispatcher
 from pythonosc import osc_server
-from pythonosc import*
 
-def position_func(addr,msg):
-    print(addr,msg)
+def print_volume_handler(unused_addr, args, volume):
+  print("[{0}] ~ {1}".format(args[0], volume))
 
+def print_compute_handler(unused_addr, args, volume):
+  try:
+    print("[{0}] ~ {1}".format(args[0], args[1](volume)))
+  except ValueError: pass
 
-dispatch = dispatcher.Dispatcher()
-dispatch.map("/position", position_func)
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--ip",
+      default="0.0.0.0", help="The ip to listen on")
+  parser.add_argument("--port",
+      type=int, default=9998, help="The port to listen on")
+  args = parser.parse_args()
+
+  dispatcher = dispatcher.Dispatcher()
+  dispatcher.map("/filter", print)
+  dispatcher.map("/volume", print_volume_handler, "Volume")
+  dispatcher.map("/logvolume", print_compute_handler, "Log volume", math.log)
+
+  server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
+  print("Serving on {}".format(server.server_address))
+  server.serve_forever()
