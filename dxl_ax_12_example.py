57aaa49d8a95f3c34b1c331dd65410da2df4fa7e
Moon <ucbqkmo@ucl.ac.uk>
2019-11-12 18:35:25 +0000

    morechanges

--- dxl_ax_12_example.py
+++ dxl_ax_12_example.py
@@ -8,20 +8,20 @@ every dxl function has to be activated by Dispatcher exept port
 
 dxl_client_osc.portOpen()
 #read position
-oscServer.dispatch_callback("/readPosition",dxl_ax_12.positionRead)
+oscServer.dispatch_callback("/readPosition",dxl_client_osc.positionRead)
 #read load
-oscServer.dispatch_callback("/readLoad",dxl_ax_12.loadRead)
+oscServer.dispatch_callback("/readLoad",dxl_client_osc.loadRead)
 #read temperature
-oscServer.dispatch_callback("/readTemp",dxl_ax_12.tempRead)
+oscServer.dispatch_callback("/readTemp",dxl_client_osc.tempRead)
 #write torque
-oscServer.dispatch_callback("/torque",dxl_ax_12.torqueEnable)
+oscServer.dispatch_callback("/torque",dxl_client_osc.torqueEnable)
 #write wheelmode
-oscServer.dispatch_callback("/wheelMode",dxl_ax_12.wheelMode)
+oscServer.dispatch_callback("/wheelMode",dxl_client_osc.wheelMode)
 #write speed
-oscServer.dispatch_callback("/writeSpeed",dxl_ax_12.servoSpeed)
+oscServer.dispatch_callback("/writeSpeed",dxl_client_osc.servoSpeed)
 #write goal position
-oscServer.dispatch_callback("/writeGoalPos",dxl_ax_12.servoGoalPos)
+oscServer.dispatch_callback("/writeGoalPos",dxl_client_osc.servoGoalPos)
 #close port
-oscServer.dispatch_callback("/closePort",dxl_ax_12.portClose)
+oscServer.dispatch_callback("/closePort",dxl_client_osc.portClose)
 #keep listening
 oscServer.server_threading(oscServer.addr, oscServer.dispatcher)
